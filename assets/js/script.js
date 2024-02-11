3f8c67ba8899f7760829397b2f4dec58b9cc4e6c
sara-hines <90005274+sara-hines@users.noreply.github.com>
2024-02-10 21:49:01 -0700

    Generate tables for results when user clicks to see breweries and events.

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,7 +96,6 @@ function getBreweries() {
 
 function getBrewery(city) {
     var requestURL = `https://api.openbrewerydb.org/v1/breweries?by_city=${city}`;
-        // console.log(requestURL);
     fetch(requestURL)
         .then(function (response) {
             return response.json();
@@ -106,8 +105,35 @@ function getBrewery(city) {
             localStorage.setItem(`brewery${city}Stored`, JSON.stringify(breweryData));
             var retrievedBrewery = JSON.parse(localStorage.getItem(`brewery${city}Stored`));
             console.log(retrievedBrewery);
+
+            // The below 3 lines create the table to display brewery results, add the class of "table" to the table, and append the table to the results container div. 
+            var breweryTable = document.createElement("table");
+            breweryTable.classList.add("table");
+            results.append(breweryTable);
+
+            // The below lines of code add a table head, a table row within the table head, and th elements within that table row to act as titles for each column of the table. 
+            var breweryHead = document.createElement("thead");
+            breweryTable.append(breweryHead);
+            var breweryHeadTr = document.createElement("tr");
+            breweryHead.append(breweryHeadTr);
+            var breweryNameTh = document.createElement("th");
+            breweryNameTh.textContent = "Brewery Name";
+            breweryHeadTr.append(breweryNameTh);
+            var breweryTypeTh = document.createElement("th");
+            breweryTypeTh.textContent = "Brewery Type";
+            breweryHeadTr.append(breweryTypeTh);
+            var breweryAddressTh = document.createElement("th");
+            breweryAddressTh.textContent = "Address";
+            breweryHeadTr.append(breweryAddressTh);
+            var breweryWebsiteTh = document.createElement("th");
+            breweryWebsiteTh.textContent = "Website";
+            breweryHeadTr.append(breweryWebsiteTh);
+
+            // The below 2 lines of code create and append a table body to the table. The retrieved data on breweries will be appended to the table body.
+            var breweryBody = document.createElement("tbody");
+            breweryTable.append(breweryBody);
+
             for (i = 0; i < retrievedBrewery.length; i++) {
-                var li = document.createElement("li");
                 if (retrievedBrewery[i].brewery_type == null) {
                     retrievedBrewery[i].brewery_type = "[unavailable]";
                 }
@@ -117,9 +143,21 @@ function getBrewery(city) {
                 if (retrievedBrewery[i].website_url == null) {
                     retrievedBrewery[i].website_url = "[unavailable]";
                 }
-                li.textContent = retrievedBrewery[i].name + ": " + retrievedBrewery[i].brewery_type + " at " + retrievedBrewery[i].address_1 + ". View the website at " + retrievedBrewery[i].website_url + ".";
-                var ul = document.querySelector("ul.brewery-results");
-                ul.appendChild(li);
+                // One table row will be created for each retrieved brewery, and table data elements will be created and appended to the table row in order to display the brewery name, type, address, and website. Each table row created will be appended to the table body.
+                var breweryTr = document.createElement("tr");
+                breweryBody.append(breweryTr);
+                var breweryNameTd = document.createElement("td");
+                breweryNameTd.textContent = retrievedBrewery[i].name;
+                breweryTr.append(breweryNameTd);
+                var breweryTypeTd = document.createElement("td");
+                breweryTypeTd.textContent = retrievedBrewery[i].brewery_type;
+                breweryTr.append(breweryTypeTd);
+                var breweryAddressTd = document.createElement("td");
+                breweryAddressTd.textContent = retrievedBrewery[i].address_1;
+                breweryTr.append(breweryAddressTd);
+                var breweryWebsiteTd = document.createElement("td");
+                breweryWebsiteTd.textContent = retrievedBrewery[i].website_url;
+                breweryTr.append(breweryWebsiteTd);
             }
         });
 }
@@ -158,7 +196,6 @@ function getEvents() {
 // Sort event from soonest to furthest out in future parameter was taken out, and is &sort=date,asc
 function getEvent(city) {
     var requestURL = `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&sort=date,asc&city=${city}&apikey=ZNzKLs0LgFjnzr7MbEoFJueWt1j24tfW`;
-        console.log(requestURL);
     fetch(requestURL)
         .then(function (response) {
             return response.json();
@@ -168,12 +205,36 @@ function getEvent(city) {
             localStorage.setItem(`event${city}Stored`, JSON.stringify(eventData));
             var retrievedEvent = JSON.parse(localStorage.getItem(`event${city}Stored`));
             console.log(retrievedEvent);
-            var ul = document.querySelector("ul.event-results");
-            var li = document.createElement("li");
-            li.textContent = city;
-            ul.appendChild(li);
+
+            // The below 3 lines create the table to display event results, add the class of "table" to the table, and append the table to the results container div. 
+            var eventTable = document.createElement("table");
+            eventTable.classList.add("table");
+            results.append(eventTable);
+
+            // The below lines of code add a table head, a table row within the table head, and th elements within that table row to act as titles for each column of the table. 
+            var eventHead = document.createElement("thead");
+            eventTable.append(eventHead);
+            var eventHeadTr = document.createElement("tr");
+            eventHead.append(eventHeadTr);
+            var eventNameTh = document.createElement("th");
+            eventNameTh.textContent = "Event";
+            eventHeadTr.append(eventNameTh);
+            var eventWhenTh = document.createElement("th");
+            eventWhenTh.textContent = "Date and Time";
+            eventHeadTr.append(eventWhenTh);
+            var eventVenueTh = document.createElement("th");
+            eventVenueTh.textContent = "Venue";
+            eventHeadTr.append(eventVenueTh);
+            var eventWebsiteTh = document.createElement("th");
+            eventWebsiteTh.textContent = "Website";
+            eventHeadTr.append(eventWebsiteTh);
+
+            // The below 2 lines of code create and append a table body to the table. The retrieved data on breweries will be appended to the table body.
+            var eventBody = document.createElement("tbody");
+            eventTable.append(eventBody);
+
             for (i = 0; i < retrievedEvent.length; i++) {
-                var li = document.createElement("li");
+
                 var unformattedDate = retrievedEvent[i].dates.start.localDate;
                 var formattedDate = dayjs(unformattedDate).format("MMM D, YYYY");
                 var unformattedTime = new Date(unformattedDate);
@@ -189,9 +250,21 @@ function getEvent(city) {
                 if (venue == null) {
                     venue = "[unavailable]";
                 }
-                li.textContent = retrievedEvent[i].name + " on " + formattedDate + " at " + formattedTime + " at " + venue;
-                var ul = document.querySelector("ul.event-results");
-                ul.appendChild(li);
+                // One table row will be created for each retrieved event, and table data elements will be created and appended to the table row in order to display the event name, date & time, venue, and website. Each table row created will be appended to the table body.
+                var eventTr = document.createElement("tr");
+                eventBody.append(eventTr);
+                var eventNameTd = document.createElement("td");
+                eventNameTd.textContent = retrievedEvent[i].name;
+                eventTr.append(eventNameTd);
+                var eventWhenTd = document.createElement("td");
+                eventWhenTd.textContent = formattedDate + " at " + formattedTime
+                eventTr.append(eventWhenTd);
+                var eventVenueTd = document.createElement("td");
+                eventVenueTd.textContent = venue;
+                eventTr.append(eventVenueTd);
+                var eventWebsiteTd = document.createElement("td");
+                eventWebsiteTd.textContent = retrievedEvent[i].url;
+                eventTr.append(eventWebsiteTd);
             }
         });
 }
